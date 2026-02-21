be6ad94c5a11aa79203314705517e28313cf5d50
adwaid-p <adwaidpaloli007@gmail.com>
2026-02-21 20:36:06 +0530

    median of array

--- median.py
+++ median.py
@@ -1,1 +1,12 @@
-arr = [4, 7, 1, 2, 5, 6]
+# Find Median of the given Array
+# Problem Statement: Given an unsorted array, find the median of the given array.
+
+arr = [4, 7, 1, 2, 5, 6, 8]
+arr.sort()
+print(arr)
+n = len(arr)
+
+if len(arr)%2 == 0:
+    print((arr[n//2 - 1] + arr[n//2])/2)
+else:
+    print(arr[n//2])
