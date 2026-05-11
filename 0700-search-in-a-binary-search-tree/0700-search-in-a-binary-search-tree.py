e3806e62ee77d6fae9d3188706b03bf9fab0ef83
Adwaid P <adwaidpaloli007@gmail.com>
2026-05-11 23:34:17 +0530

    Time: 0 ms (100%), Space: 21 MB (7.66%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
@@ -6,6 +6,7 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
         if not root:
             return None
         
