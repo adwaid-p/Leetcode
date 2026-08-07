58bf17f71c2e896e0aab68ebf6db1e65f3cf8a34
Adwaid P <adwaidpaloli007@gmail.com>
2026-08-07 20:28:55 +0530

    Time: 11 ms (40.87%), Space: 20 MB (69.76%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,25 +6,24 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        self.paths = 0        
 
-        def dfs(node, resultPath):
+        def dfs(node, currentSum, prefix_map):
             if not node:
                 return 0
-            
-            resultPath.append(node.val)
 
-            i = len(resultPath) - 1
-            currSum = 0
-            while i >= 0:
-                currSum += resultPath[i]
-                if currSum == targetSum:
-                    self.paths += 1
-                i -= 1
+            currentSum += node.val
+            target = currentSum - targetSum
+
+            count = prefix_map.get(target, 0)
             
-            dfs(node.left, resultPath)
-            dfs(node.right, resultPath)
-            resultPath.pop()
+            prefix_map[currentSum] = prefix_map.get(currentSum, 0) + 1
+
+            count += dfs(node.left, currentSum, prefix_map)
+            count += dfs(node.right, currentSum, prefix_map)
+
+            prefix_map[currentSum] -= 1
+            currentSum -= node.val
+
+            return count
         
-        dfs(root, [])
-        return self.paths
+        return dfs(root, 0, {0: 1})
